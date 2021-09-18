4bf53fe54aadc17523be570cc38993c37d77e71f
kikikrittamate <krittamate.ch@ku.th>
2021-09-18 21:49:20 +0700

    fix bugs in functions

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('date expired')
+    end_date = models.DateTimeField('date expired', null=True)
 
     def __str__(self):
         return self.question_text
@@ -22,16 +22,18 @@ class Question(models.Model):
 
     def is_published(self):
         now = timezone.now()
-        return self.pub_date <= now
+        return now >= self.pub_date
 
     def can_vote(self):
-        return self.is_published() and not self.end_date
+        now = timezone.now()
+        return self.end_date >= now >= self.pub_date
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
 
