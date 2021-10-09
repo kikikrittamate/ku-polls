f7241eb25379048ca0eb68f5c3115bfc56957657
kikikrittamate <krittamate.ch@ku.th>
2021-10-10 01:33:41 +0700

    fix bug

--- polls/tests.py
+++ polls/tests.py
@@ -183,7 +183,7 @@ class QuestionDetailViewTests(TestCase):
                                           days=5)
         url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 302)
 
     def test_past_question(self):
         """The detail view of a question with a pub_date in the past \
