9373f22f89649f807bc374680db0f2586abeb02e
kikikrittamate <krittamate.ch@ku.th>
2021-10-10 00:41:54 +0700

    finished revising the code for flake8

--- polls/admin.py
+++ polls/admin.py
@@ -1,21 +1,30 @@
+"""Create admin page."""
+
 from django.contrib import admin
 
 from .models import Choice, Question
 
 
 class ChoiceInline(admin.TabularInline):
+    """Admin can add the choice in question in default 3 fields."""
+
     model = Choice
     extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    """Admin can custom the question in admin page."""
+
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date', 'end_date'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['pub_date', 'end_date'],
+                              'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently', 'is_published', 'can_vote')
+    list_display = ('question_text', 'pub_date', 'was_published_recently',
+                    'is_published', 'can_vote')
     list_filter = ['pub_date']
     search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
